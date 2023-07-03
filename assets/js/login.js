61cbce948041428cab17a3050623efb85e560896
GabyEspR <gaby.esp.r@gmail.com>
2023-07-03 18:01:47 -0300

    Modificacion con VUE

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,70 @@
-const loginForm = document.querySelector('#loginForm')
+// const loginForm = document.querySelector('#loginForm')
 
-loginForm.addEventListener('submit', (e)=> {
-    e.preventDefault()
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user => user.email === email && user.password === password)
-    if (!validUser) {
-        return alert('Usuario y/o Contraseña incorrectos!')
-    }
-    alert(`Bienvenido ${validUser.name}`)
-    localStorage.setItem('login_success', JSON.stringify(validUser))
-    window.location.href = 'index.html'
-})
+// loginForm.addEventListener('submit', (e)=> {
+//     e.preventDefault()
+//     const email = document.querySelector('#email').value
+//     const password = document.querySelector('#password').value
+//     const Users = JSON.parse(localStorage.getItem('users')) || []
+//     const validUser = Users.find(user => user.email === email && user.password === password)
+//     if (!validUser) {
+//         return alert('Usuario y/o Contraseña incorrectos!')
+//     }
+//     alert(`Bienvenido ${validUser.name}`)
+//     localStorage.setItem('login_success', JSON.stringify(validUser))
+//     window.location.href = 'index.html'
+// })
 
+const { createApp } = Vue
+    createApp({
+        data() {
+            return {
+                clientes:[],
+                url:'https://acuastel.pythonanywhere.com/clientes',
+                error:false,
+                cargando:true,
+                usuario:"",
+                nombre:"",
+                apellido:"",
+                direccion:"",
+                telefono:"",
+                email:"",
+                contrasena:"",
+                saldo:"",
+            }
+        },
+        methods: {
+            fetchData(url) {                
+            },
+            ingresar(){
+                let clientes = {
+                    usuario: this.usuario,
+                    nombre: this.nombre,
+                    apellido: this.apellido,
+                    direccion: this.direccion,
+                    telefono: this.telefono,
+                    email: this.email,
+                    contrasena: this.contrasena,
+                    saldo: this.saldo
+                }
+                var options = {
+                    body: JSON.stringify(clientes),
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    redirect: 'follow'
+                }
+                fetch(this.url, options)
+                    .then(function () {
+                        alert("Usuario Registrado")
+                        window.location.href = "./login.html"
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        alert("Error al registrar usuario")
+                    })
+            }
+
+        },
+        created() {
+            this.fetchData(this.url)
+        },
+    }).mount('#app')
--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,18 +1,73 @@
-const signupForm = document.querySelector('#signupForm')
-signupForm.addEventListener('submit', (e)=> {
-    e.preventDefault()
-    const name = document.querySelector('#username').value
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
+// const signupForm = document.querySelector('#signupForm')
+// signupForm.addEventListener('submit', (e)=> {
+//     e.preventDefault()
+//     const name = document.querySelector('#username').value
+//     const email = document.querySelector('#email').value
+//     const password = document.querySelector('#password').value
 
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserRegistered = Users.find(user => user.email === email)
-    if (isUserRegistered) {
-        return alert('El usuario ya está registrado!')
-    }
+//     const Users = JSON.parse(localStorage.getItem('users')) || []
+//     const isUserRegistered = Users.find(user => user.email === email)
+//     if (isUserRegistered) {
+//         return alert('El usuario ya está registrado!')
+//     }
 
-    Users.push({name: name, email: email, password: password})
-    localStorage.setItem('users', JSON.stringify(Users))
-    alert('Registro Exitoso!')
-    window.location.href = 'login.html'
-})
+//     Users.push({name: name, email: email, password: password})
+//     localStorage.setItem('users', JSON.stringify(Users))
+//     alert('Registro Exitoso!')
+//     window.location.href = 'login.html'
+// })
+
+const { createApp } = Vue
+    createApp({
+        data() {
+            return {
+                clientes:[],
+                url:'https://acuastel.pythonanywhere.com/clientes',
+                error:false,
+                cargando:true,
+                usuario:"",
+                nombre:"",
+                apellido:"",
+                direccion:"",
+                telefono:"",
+                email:"",
+                contrasena:"",
+                saldo:"",
+            }
+        },
+        methods: {
+            fetchData(url) {                
+            },
+            grabar(){
+                let clientes = {
+                    usuario: this.usuario,
+                    nombre: this.nombre,
+                    apellido: this.apellido,
+                    direccion: this.direccion,
+                    telefono: this.telefono,
+                    email: this.email,
+                    contrasena: this.contrasena,
+                    saldo: this.saldo
+                }
+                var options = {
+                    body: JSON.stringify(clientes),
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    redirect: 'follow'
+                }
+                fetch(this.url, options)
+                    .then(function () {
+                        alert("Usuario Registrado")
+                        window.location.href = "./login.html"
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        alert("Error al registrar usuario")
+                    })
+            }
+
+        },
+        created() {
+            this.fetchData(this.url)
+        },
+    }).mount('#app')
